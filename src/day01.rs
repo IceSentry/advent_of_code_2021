bb27a289cb346a7f02f16aff834187afa708de82
IceSentry <c.giguere42@gmail.com>
2021-12-01 00:16:09 -0500

    day01

--- src/day01.rs
+++ src/day01.rs
@@ -6,12 +6,30 @@ pub fn parse(input: &str) -> Data {
 
 #[allow(clippy::ptr_arg)]
 pub fn part_1(input: &Data) -> usize {
-    0
+    let mut last = 0;
+    let mut count = 0;
+    for &value in input.iter().skip(1) {
+        if value > last {
+            count += 1;
+        }
+        last = value;
+    }
+    count
 }
 
 #[allow(clippy::ptr_arg)]
 pub fn part_2(input: &Data) -> usize {
-    0
+    let mut head = 0;
+    let mut tail = 0;
+    let mut count = 0;
+    for (i, values) in input.windows(3).enumerate() {
+        tail = values.iter().sum();
+        if i > 0 && tail > head {
+            count += 1;
+        }
+        head = tail;
+    }
+    count
 }
 
 #[cfg(test)]
@@ -19,19 +37,29 @@ mod tests {
     use indoc::indoc;
 
     const INPUTS: &str = indoc! {"
+        199
+        200
+        208
+        210
+        200
+        207
+        240
+        269
+        260
+        263
     "};
 
     #[test]
     pub fn part_1() {
         let input = super::parse(INPUTS);
         let result = super::part_1(&input);
-        assert_eq!(result, 1);
+        assert_eq!(result, 7);
     }
 
     #[test]
     pub fn part_2() {
         let input = super::parse(INPUTS);
         let result = super::part_2(&input);
-        assert_eq!(result, 2);
+        assert_eq!(result, 5);
     }
 }

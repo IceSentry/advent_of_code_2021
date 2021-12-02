0748f99e776b42e4827f58f158b3422efb6bcb3c
IceSentry <c.giguere42@gmail.com>
2021-12-02 00:57:24 -0500

    day02 with enums

--- src/day02.rs
+++ src/day02.rs
@@ -1,11 +1,20 @@
+use serde_derive::Deserialize;
 use serde_scan::scan;
 
-type Data = Vec<(String, i32)>;
+type Data = Vec<(Commands, i32)>;
+
+#[derive(Deserialize, Debug, PartialEq)]
+#[serde(rename_all = "snake_case")]
+pub enum Commands {
+    Forward,
+    Down,
+    Up,
+}
 
 pub fn parse(input: &str) -> Data {
     input
         .lines()
-        .map(|line| scan!("{} {}" <- line).unwrap())
+        .map(|line| scan!("{} {}" <- line).expect("Failed to parse input"))
         .collect()
 }
 
@@ -13,11 +22,10 @@ pub fn part_1(input: &Data) -> i32 {
     let mut x = 0;
     let mut y = 0;
     for (command, value) in input {
-        match command.as_ref() {
-            "forward" => x += value,
-            "down" => y += value,
-            "up" => y -= value,
-            _ => panic!("unkknown command {}", command),
+        match command {
+            Commands::Forward => x += value,
+            Commands::Down => y += value,
+            Commands::Up => y -= value,
         }
     }
     x * y
@@ -26,11 +34,10 @@ pub fn part_1(input: &Data) -> i32 {
 pub fn part_1_iterator(input: &Data) -> i32 {
     let (x, y) = input
         .iter()
-        .fold((0, 0), |(x, y), (command, value)| match command.as_ref() {
-            "forward" => (x + value, y),
-            "down" => (x, y + value),
-            "up" => (x, y - value),
-            _ => (x, y),
+        .fold((0, 0), |(x, y), (command, value)| match command {
+            Commands::Forward => (x + value, y),
+            Commands::Down => (x, y + value),
+            Commands::Up => (x, y - value),
         });
     x * y
 }
@@ -40,30 +47,27 @@ pub fn part_2(input: &Data) -> i32 {
     let mut y = 0;
     let mut aim = 0;
     for (command, value) in input {
-        match command.as_ref() {
-            "forward" => {
+        match command {
+            Commands::Forward => {
                 x += value;
                 y += aim * value;
             }
-            "down" => aim += value,
-            "up" => aim -= value,
-            _ => panic!("unkknown command {}", command),
+            Commands::Down => aim += value,
+            Commands::Up => aim -= value,
         }
     }
     x * y
 }
 
 pub fn part_2_iterator(input: &Data) -> i32 {
-    let (x, y, _aim) = input
-        .iter()
-        .fold((0, 0, 0), |(x, y, aim), (command, value)| {
-            match command.as_ref() {
-                "forward" => (x + value, y + aim * value, aim),
-                "down" => (x, y, aim + value),
-                "up" => (x, y, aim - value),
-                _ => (x, y, aim),
-            }
-        });
+    let (x, y, _aim) =
+        input
+            .iter()
+            .fold((0, 0, 0), |(x, y, aim), (command, value)| match command {
+                Commands::Forward => (x + value, y + aim * value, aim),
+                Commands::Down => (x, y, aim + value),
+                Commands::Up => (x, y, aim - value),
+            });
     x * y
 }
 

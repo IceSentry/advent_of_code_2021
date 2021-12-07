e19bc9bb3841f48cbe9f7ae198ab3601f73e2a0e
IceSentry <c.giguere42@gmail.com>
2021-12-07 10:19:39 -0500

    day07 remove memo

--- src/day07.rs
+++ src/day07.rs
@@ -1,5 +1,3 @@
-use hashbrown::HashMap;
-
 type Data = Vec<isize>;
 
 pub fn parse(input: &str) -> Data {
@@ -27,18 +25,12 @@ pub fn part_1(input: &Data) -> usize {
 pub fn part_2(input: &Data) -> usize {
     let max = input.iter().max().unwrap();
     let mut vec = vec![];
-    let mut map: HashMap<(isize, isize), isize> = HashMap::new();
     for i in 0..*max {
         vec.push(0);
         for crab in input {
             let dist = (*crab - i).abs();
-            vec[i as usize] += if let Some(fuel) = map.get(&(*crab, dist)) {
-                *fuel
-            } else {
-                let fuel = dist * (dist + 1) / 2;
-                map.insert((*crab, dist), fuel);
-                fuel
-            }
+            // memoizing this is way slower because most numbers are not repeated
+            vec[i as usize] += dist * (dist + 1) / 2;
         }
     }
     *vec.iter().min().unwrap() as usize

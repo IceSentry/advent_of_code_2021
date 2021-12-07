1fe6c1048ae77cf007dcd71bc76d7a6946834563
IceSentry <c.giguere42@gmail.com>
2021-12-07 10:48:23 -0500

    day07 optimal imple

--- src/day07.rs
+++ src/day07.rs
@@ -22,18 +22,49 @@ pub fn part_1(input: &Data) -> usize {
     *vec.iter().min().unwrap() as usize
 }
 
+pub fn part_1_median(input: &Data) -> usize {
+    let mut input = input.clone();
+    input.sort_unstable();
+    let median = input[(input.len() / 2) as usize];
+    let mut sum = 0;
+    for crab in input {
+        sum += (crab - median).abs();
+    }
+    sum as usize
+}
+
 pub fn part_2(input: &Data) -> usize {
     let max = input.iter().max().unwrap();
     let mut vec = vec![];
     for i in 0..*max {
         vec.push(0);
         for crab in input {
-            let dist = (*crab - i).abs();
-            // memoizing this is way slower because most numbers are not repeated
-            vec[i as usize] += dist * (dist + 1) / 2;
+            let dist = *crab - i;
+            let fuel = (dist.pow(2) + dist.abs()) / 2;
+            vec[i as usize] += fuel;
         }
     }
     *vec.iter().min().unwrap() as usize
+}
+
+pub fn part_2_mean(input: &Data) -> usize {
+    let n = input.len() as isize;
+    let mean = input.iter().sum::<isize>() / n;
+    let fuel = |dist: isize| (dist.pow(2) + dist.abs()) / 2;
+
+    let mut sum_1 = 0;
+    for crab in input {
+        let dist = (crab - (mean as f32).floor() as isize).abs();
+        sum_1 += fuel(dist);
+    }
+
+    let mut sum_2 = 0;
+    for crab in input {
+        let dist = (crab - (mean as f32).ceil() as isize).abs();
+        sum_2 += fuel(dist);
+    }
+
+    sum_1.min(sum_2) as usize
 }
 
 #[cfg(test)]
--- src/main.rs
+++ src/main.rs
@@ -6,5 +6,5 @@ aoc_helper::main! {
     day04: parse => part_1, part_2;
     day05: parse => part_1, part_2;
     day06: parse => part_1, part_2;
-    day07: parse => part_1, part_2;
+    day07: parse => part_1, part_1_median, part_2, part_2_mean;
 }

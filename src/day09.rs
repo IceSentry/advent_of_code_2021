a44b9ccca922be8c920896815da745900548001e
IceSentry <c.giguere42@gmail.com>
2021-12-09 23:42:32 -0500

    day09 clean up

--- src/day09.rs
+++ src/day09.rs
@@ -4,6 +4,8 @@ use hashbrown::HashSet;
 
 type Data = Map;
 
+const NEIGHBOURS: [(isize, isize); 4] = [(0, 1), (1, 0), (0, -1), (-1, 0)];
+
 pub struct Map {
     data: Vec<Vec<u32>>,
     cols: usize,
@@ -17,6 +19,33 @@ impl Map {
         } else {
             None
         }
+    }
+
+    fn get_i(&self, x: isize, y: isize) -> Option<&u32> {
+        if let Some(row) = self.data.get(y as usize) {
+            row.get(x as usize)
+        } else {
+            None
+        }
+    }
+
+    fn get_neighbours(&self, pos: (usize, usize)) -> Vec<((usize, usize), u32)> {
+        NEIGHBOURS
+            .iter()
+            .map(|(n_x, n_y)| (pos.0 as isize + n_x, pos.1 as isize + n_y))
+            .filter_map(|(n_x, n_y)| {
+                self.get_i(n_x, n_y)
+                    .map(|v| ((n_x as usize, n_y as usize), *v))
+            })
+            .collect()
+    }
+
+    fn check_neighbours(&self, at: (usize, usize), value: &u32) -> bool {
+        NEIGHBOURS
+            .iter()
+            .map(|(n_x, n_y)| (at.0 as isize + n_x, at.1 as isize + n_y))
+            .filter_map(|(x, y)| self.get_i(x, y))
+            .all(|n| n > value)
     }
 }
 
@@ -34,51 +63,12 @@ pub fn parse(input: &str) -> Data {
     }
 }
 
-// 1D array version doesn't work for some reason
-// fn check_neighbours(map: &Map, at: usize, value: &u32) -> bool {
-//     let mut neighbours: Vec<isize> = vec![map.cols as isize, -(map.cols as isize)];
-//     let div = at / map.cols;
-//     if div == map.cols {
-//         neighbours.push(-1);
-//     } else if div == 0 {
-//         neighbours.push(1);
-//     } else {
-//         neighbours.push(1);
-//         neighbours.push(-1);
-//     }
-//     neighbours
-//         .iter()
-//         .map(|offset| at as isize + offset)
-//         .filter_map(|index| map.data.get(index as usize))
-//         .all(|n| n > value)
-// }
-
-// pub fn part_1(input: &Data) -> u32 {
-//     let mut results = vec![];
-//     for (i, value) in input.data.iter().enumerate() {
-//         if check_neighbours(input, i, value) {
-//             results.push(value + 1);
-//         }
-//     }
-//     results.iter().sum()
-// }
-
-const NEIGHBOURS: [(isize, isize); 4] = [(0, 1), (1, 0), (0, -1), (-1, 0)];
-
-fn check_neighbours(map: &Map, at_x: usize, at_y: usize, value: &u32) -> bool {
-    NEIGHBOURS
-        .iter()
-        .map(|(n_x, n_y)| (at_x as isize + n_x, at_y as isize + n_y))
-        .filter_map(|(x, y)| map.get(x as usize, y as usize))
-        .all(|n| n > value)
-}
-
 pub fn part_1(input: &Data) -> u32 {
     let mut results = vec![];
     for y in 0..input.rows {
         for x in 0..input.cols {
             let value = input.data[y][x];
-            if check_neighbours(input, x, y, &value) {
+            if input.check_neighbours((x, y), &value) {
                 results.push(value + 1);
             }
         }
@@ -91,47 +81,28 @@ pub fn part_2(input: &Data) -> u32 {
     for y in 0..input.rows {
         for x in 0..input.cols {
             let value = input.data[y][x];
-            if check_neighbours(input, x, y, &value) {
-                // println!("looking for basin from ({}, {}): {}", x, y, value);
-                basins.push(find_basin_size(input, x, y));
-                // println!();
+            if input.check_neighbours((x, y), &value) {
+                basins.push(find_basin_size(input, (x, y)));
             }
         }
     }
     basins.sort_unstable();
-    // println!("{:?}", basins);
     basins.iter().rev().take(3).product::<usize>() as u32
 }
 
-fn get_neighbours(map: &Map, x: usize, y: usize) -> Vec<((usize, usize), u32)> {
-    NEIGHBOURS
-        .iter()
-        .map(|(n_x, n_y)| (x as isize + n_x, y as isize + n_y))
-        .filter_map(|(n_x, n_y)| {
-            map.get(n_x as usize, n_y as usize)
-                .map(|v| ((n_x as usize, n_y as usize), *v))
-        })
-        .collect()
-}
-
-fn find_basin_size(map: &Map, x: usize, y: usize) -> usize {
+fn find_basin_size(map: &Map, root: (usize, usize)) -> usize {
     let mut queue = VecDeque::new();
     let mut checked = HashSet::new();
-    checked.insert((x, y));
-    queue.push_front((x, y));
-
+    checked.insert(root);
+    queue.push_front(root);
     while !queue.is_empty() {
-        let pos = queue.pop_back().expect("queue should not be empty");
-        if let Some(v) = map.get(pos.0, pos.1) {
-            // println!("checking neighbours of {:?}: {}", pos, v);
-            for (n_pos, n_v) in get_neighbours(map, pos.0, pos.1) {
-                // print!("{:?}: {}", n_pos, n_v);
+        let curr = queue.pop_back().expect("queue should not be empty");
+        if let Some(v) = map.get(curr.0, curr.1) {
+            for (n_pos, n_v) in map.get_neighbours(curr) {
                 if n_v != 9 && &n_v > v && !checked.contains(&n_pos) {
-                    // print!(" enqueue");
                     checked.insert(n_pos);
                     queue.push_front(n_pos);
                 }
-                // println!();
             }
         }
     }

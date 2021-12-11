f768054a1865e5d99f9fb481226c2c21ed3ff3f3
IceSentry <c.giguere42@gmail.com>
2021-12-11 18:09:24 -0500

    day11

---
+++ src/day11.rs
@@ -0,0 +1,138 @@
+use colored::Colorize;
+
+type Data = Vec<Vec<i32>>;
+
+pub fn parse(input: &str) -> Data {
+    input
+        .lines()
+        .map(|l| {
+            l.chars()
+                .flat_map(|c| c.to_digit(10))
+                .map(|x| x as i32)
+                .collect()
+        })
+        .collect()
+}
+
+const NEIGHBOURS: [(isize, isize); 8] = [
+    (0, 1),
+    (0, -1),
+    (1, 0),
+    (1, 1),
+    (1, -1),
+    (-1, 0),
+    (-1, 1),
+    (-1, -1),
+];
+
+fn print_data(data: &Data) {
+    for row in data {
+        for val in row {
+            if *val == 0 {
+                print!("{}", val.to_string().yellow());
+            } else {
+                print!("{}", val);
+            }
+        }
+        println!();
+    }
+    println!();
+}
+
+fn step(data: &mut Data) -> usize {
+    let mut flashes = 0;
+    for rows in data.iter_mut() {
+        for value in rows.iter_mut() {
+            *value += 1;
+        }
+    }
+
+    let mut keep_running = true;
+    while keep_running {
+        keep_running = false;
+        for y in 0..10 {
+            for x in 0..10 {
+                if data[y][x] > 9 {
+                    flashes += 1;
+                    data[y][x] = 0;
+                    for (n_x, n_y) in NEIGHBOURS {
+                        if let Some(val) = data
+                            .get_mut((y as isize + n_y) as usize)
+                            .and_then(|row| row.get_mut((x as isize + n_x) as usize))
+                        {
+                            if *val <= 9 && *val != 0 {
+                                *val += 1;
+                                keep_running = true;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    flashes
+}
+
+pub fn part_1(input: &Data) -> usize {
+    let mut data = input.clone();
+    let mut flashes = 0;
+    for _ in 0..100 {
+        flashes += step(&mut data);
+    }
+    flashes
+}
+
+pub fn part_2(input: &Data) -> usize {
+    let mut data = input.clone();
+    let mut i = 0;
+    loop {
+        i += 1;
+        step(&mut data);
+        if data.iter().all(|row| row.iter().all(|val| *val == 0)) {
+            return i;
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::day11::step;
+    use indoc::indoc;
+
+    const INPUTS: &str = indoc! {"
+        5483143223
+        2745854711
+        5264556173
+        6141336146
+        6357385478
+        4167524645
+        2176841721
+        6882881134
+        4846848554
+        5283751526
+    "};
+
+    #[test]
+    pub fn step_test() {
+        let mut input = super::parse(INPUTS);
+        let mut flashes = 0;
+        for _ in 0..10 {
+            flashes += step(&mut input);
+        }
+        assert_eq!(flashes, 204);
+    }
+
+    #[test]
+    pub fn part_1() {
+        let input = super::parse(INPUTS);
+        let result = super::part_1(&input);
+        assert_eq!(result, 1656);
+    }
+
+    #[test]
+    pub fn part_2() {
+        let input = super::parse(INPUTS);
+        let result = super::part_2(&input);
+        assert_eq!(result, 195);
+    }
+}

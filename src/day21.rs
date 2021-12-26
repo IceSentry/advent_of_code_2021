147a83a1a8ca21a02d13b53f7c14d6861b527222
IceSentry <c.giguere42@gmail.com>
2021-12-26 16:28:58 -0500

    day21 improve performance

--- src/day21.rs
+++ src/day21.rs
@@ -3,33 +3,33 @@ use std::{iter::Cycle, ops::RangeInclusive};
 use hashbrown::HashMap;
 use serde_scan::scan;
 
-type Data = (usize, usize);
-type Die = Cycle<RangeInclusive<usize>>;
+type Data = (u16, u16);
+type Die = Cycle<RangeInclusive<u16>>;
 
 #[derive(Clone, Copy, PartialEq, Eq, Hash)]
 struct Player {
-    position: usize,
-    score: usize,
+    position: u16,
+    score: u16,
 }
 
 impl Player {
-    fn new(position: usize) -> Self {
+    fn new(position: u16) -> Self {
         Self { position, score: 0 }
     }
 
-    fn advance_mut(&mut self, amount: usize) {
+    fn advance_mut(&mut self, amount: u16) {
         self.position = (self.position + amount - 1) % 10 + 1;
         self.score += self.position;
     }
 
-    fn advance(&self, amount: usize) -> Self {
-        let position = (self.position + amount - 1) % 10 + 1;
-        let score = self.score + position;
-        Self { position, score }
+    fn advance(&self, amount: u16) -> Self {
+        let mut new_player = *self;
+        new_player.advance_mut(amount);
+        new_player
     }
 }
 
-fn roll(die: &mut Die, roll_count: &mut usize) -> usize {
+fn roll(die: &mut Die, roll_count: &mut usize) -> u16 {
     let mut roll_sum = 0;
     for _ in 0..3 {
         roll_sum += die.next().expect("Cycle should be infinite");
@@ -38,11 +38,41 @@ fn roll(die: &mut Die, roll_count: &mut usize) -> usize {
     roll_sum
 }
 
+const POSSIBLE_ROLLS: [(u16, usize); 7] = [(3, 1), (4, 3), (5, 6), (6, 7), (7, 6), (8, 3), (9, 1)];
+fn count_wins(
+    cache: &mut HashMap<(Player, Player), (usize, usize)>,
+    player_a: Player,
+    player_b: Player,
+) -> (usize, usize) {
+    let key = (player_a, player_b);
+
+    if let Some(value) = cache.get(&key) {
+        return *value;
+    }
+
+    let mut player_a_wins = 0;
+    let mut player_b_wins = 0;
+    for (value, count) in POSSIBLE_ROLLS {
+        let new_player_a = player_a.advance(value);
+        if new_player_a.score >= 21 {
+            player_a_wins += count;
+        } else {
+            let (player_b_win, player_a_win) = count_wins(cache, player_b, new_player_a);
+            player_a_wins += player_a_win * count;
+            player_b_wins += player_b_win * count;
+        }
+    }
+
+    let result = (player_a_wins, player_b_wins);
+    cache.insert(key, result);
+    result
+}
+
 pub fn parse(input: &str) -> Data {
     let players = input
         .lines()
         .flat_map(|l| scan!("Player {} starting position: {}" <- l))
-        .collect::<Vec<(usize, usize)>>();
+        .collect::<Vec<(u16, u16)>>();
 
     (players[0].1, players[1].1)
 }
@@ -59,55 +89,24 @@ pub fn part_1(input: &Data) -> usize {
     loop {
         player_1.advance_mut(roll(&mut die, &mut roll_count));
         if player_1.score >= max_score {
-            return player_2.score * roll_count;
+            return player_2.score as usize * roll_count;
         }
 
         player_2.advance_mut(roll(&mut die, &mut roll_count));
         if player_2.score >= max_score {
-            return player_1.score * roll_count;
+            return player_1.score as usize * roll_count;
         }
     }
-}
-
-fn compute_win(
-    cache: &mut HashMap<(Player, Player), (usize, usize)>,
-    player_a: Player,
-    player_b: Player,
-) -> (usize, usize) {
-    let key = (player_a, player_b);
-    if cache.contains_key(&key) {
-        return cache[&key];
-    }
-
-    let mut player_a_wins = 0;
-    let mut player_b_wins = 0;
-    for a in 1..=3 {
-        for b in 1..=3 {
-            for c in 1..=3 {
-                let player_a = player_a.advance(a + b + c);
-                if player_a.score >= 21 {
-                    player_a_wins += 1;
-                } else {
-                    let (player_b_win, player_a_win) = compute_win(cache, player_b, player_a);
-                    player_a_wins += player_a_win;
-                    player_b_wins += player_b_win;
-                }
-            }
-        }
-    }
-
-    let result = (player_a_wins, player_b_wins);
-    cache.insert(key, result);
-    result
 }
 
 pub fn part_2(input: &Data) -> usize {
     let (player_1_pos, player_2_pos) = input;
-    let player_1 = Player::new(*player_1_pos);
-    let player_2 = Player::new(*player_2_pos);
-
     let mut cache = HashMap::new();
-    let (player_1_wins, player_2_wins) = compute_win(&mut cache, player_1, player_2);
+    let (player_1_wins, player_2_wins) = count_wins(
+        &mut cache,
+        Player::new(*player_1_pos),
+        Player::new(*player_2_pos),
+    );
     player_1_wins.max(player_2_wins)
 }
 

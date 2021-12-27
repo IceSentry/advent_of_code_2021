f3b375a7808c67db492368a42ab131b61c483851
IceSentry <c.giguere42@gmail.com>
2021-12-26 21:14:16 -0500

    improve day20 performance

--- src/day20.rs
+++ src/day20.rs
@@ -1,18 +1,20 @@
 use hashbrown::HashSet;
 
-type Data = (Vec<char>, HashSet<(isize, isize)>);
+type Image = HashSet<u32>;
+type Enhancer = Vec<u8>;
+type Data = (Enhancer, Image);
 
-fn _print_image(image: &HashSet<(isize, isize)>) {
-    let mut min_x = isize::MAX;
-    let mut min_y = isize::MAX;
-    let mut max_x = isize::MIN;
-    let mut max_y = isize::MIN;
+fn _print_image(image: &Image) {
+    let mut min_x = i16::MAX;
+    let mut min_y = i16::MAX;
+    let mut max_x = i16::MIN;
+    let mut max_y = i16::MIN;
 
-    for (x, y) in image {
-        min_x = *x.min(&min_x);
-        min_y = *y.min(&min_y);
-        max_x = *x.max(&max_x);
-        max_y = *y.max(&max_y);
+    for (x, y) in image.iter().map(|pixel| unpack(*pixel)) {
+        min_x = x.min(min_x);
+        min_y = y.min(min_y);
+        max_x = x.max(max_x);
+        max_y = y.max(max_y);
     }
 
     min_x -= 1;
@@ -22,18 +24,19 @@ fn _print_image(image: &HashSet<(isize, isize)>) {
 
     for y in min_y..=max_y {
         for x in min_x..=max_x {
-            print!("{}", if image.contains(&(x, y)) { '#' } else { '.' });
+            let pixel = pack(x, y);
+            print!("{}", if image.contains(&pixel) { '#' } else { '.' });
         }
         println!();
     }
 }
 
-fn get_enhance_index(image: &HashSet<(isize, isize)>, x: isize, y: isize, invert: bool) -> usize {
+fn get_enhance_index(image: &Image, x: i16, y: i16, invert: bool) -> usize {
     let mut enhance_index = 0;
     for n_y in -1..=1 {
         for n_x in -1..=1 {
             enhance_index <<= 1;
-            if image.contains(&(x + n_x, y + n_y)) {
+            if image.contains(&pack(x + n_x, y + n_y)) {
                 enhance_index |= invert as usize;
             } else {
                 enhance_index |= !invert as usize;
@@ -43,25 +46,20 @@ fn get_enhance_index(image: &HashSet<(isize, isize)>, x: isize, y: isize, invert
     enhance_index
 }
 
-fn enhance(
-    image: &HashSet<(isize, isize)>,
-    enhancer: &[char],
-    i: usize,
-) -> HashSet<(isize, isize)> {
+fn enhance(image: &Image, enhancer: &[u8], i: usize) -> Image {
     let mut next_image = HashSet::new();
     let mut visited = HashSet::new();
-    let invert = enhancer[0] == '#' && i % 2 == 0;
-    let match_char = if invert { '.' } else { '#' };
+    let invert = enhancer[0] == b'#' && i % 2 == 0;
+    let match_char = if invert { b'.' } else { b'#' };
+    let invert = enhancer[0] != b'#' || invert;
 
-    for (x, y) in image {
+    for (x, y) in image.iter().map(|pixel| unpack(*pixel)) {
         for dy in -1..=1 {
             for dx in -1..=1 {
-                let pixel = (*x + dx, *y + dy);
+                let pixel = pack(x + dx, y + dy);
                 if !visited.contains(&pixel) {
                     visited.insert(pixel);
-                    let enhance_index =
-                        get_enhance_index(image, x + dx, y + dy, enhancer[0] != '#' || invert);
-                    if enhancer[enhance_index] == match_char {
+                    if enhancer[get_enhance_index(image, x + dx, y + dy, invert)] == match_char {
                         next_image.insert(pixel);
                     }
                 }
@@ -74,22 +72,19 @@ fn enhance(
 pub fn parse(input: &str) -> Data {
     let (image_enhancement_algorithm, raw_image) = input.split_once("\n\n").unwrap();
     let mut image_set = HashSet::new();
-    let image: Vec<Vec<char>> = raw_image.lines().map(|l| l.chars().collect()).collect();
+    let image: Vec<Vec<u8>> = raw_image.lines().map(|l| l.bytes().collect()).collect();
     let y_offset = image.len() / 2;
     let x_offset = image[0].len() / 2;
 
     for (y, row) in image.iter().enumerate() {
         for (x, pixel) in row.iter().enumerate() {
-            if *pixel == '#' {
-                image_set.insert((
-                    x as isize - x_offset as isize,
-                    y as isize - y_offset as isize,
-                ));
+            if *pixel == b'#' {
+                image_set.insert(pack(x as i16 - x_offset as i16, y as i16 - y_offset as i16));
             }
         }
     }
 
-    (image_enhancement_algorithm.chars().collect(), image_set)
+    (image_enhancement_algorithm.bytes().collect(), image_set)
 }
 
 pub fn part_1(input: &Data) -> usize {
@@ -108,6 +103,17 @@ pub fn part_2(input: &Data) -> usize {
         next_image = enhance(&next_image, enhancer, i);
     }
     next_image.len()
+}
+
+fn pack(x: i16, y: i16) -> u32 {
+    ((x as u32) << 16) | (y as u32 & 0x0000FFFF)
+}
+
+fn unpack(pixel: u32) -> (i16, i16) {
+    (
+        ((pixel & 0xFFFF0000) >> 16) as i16,
+        (pixel & 0x0000FFFF) as i16,
+    )
 }
 
 #[cfg(test)]
@@ -138,6 +144,16 @@ mod tests {
             .collect::<Vec<_>>()
             .join("");
         format!("{}\n\n{}", enhance, image)
+    }
+
+    #[test]
+    pub fn pack() {
+        let x_expected: i16 = 42;
+        let y_expected: i16 = -42;
+        let pixel = super::pack(x_expected, y_expected);
+        let (x, y) = super::unpack(pixel);
+        assert_eq!(x, x_expected);
+        assert_eq!(y, y_expected);
     }
 
     #[test]
